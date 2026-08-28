a9e443f3dc90af1abb0a88f3dbcd5e5ad1cc450e
Pedro Henrique <ph.andradesousa31@gmail.com>
2026-08-28 01:38:15 -0300

    feat: organize archives, modify/create components

--- app/(board)/page.tsx
+++ app/(board)/page.tsx
@@ -1,12 +1,10 @@
 import { ArchiveIcon, MessageCircleIcon, ThumbsUp } from "lucide-react";
 import { Section } from "@/components/section";
 import { Card } from "@/components/card";
+import { Button } from "@/components/button";
 
 export default function Home() {
   return (
-    <div className="max-w-[1620px] w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
-      <div />
-
       <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
         <Section.Root>
           <Section.Header>
@@ -25,19 +23,18 @@ export default function Home() {
                   <Card.Title>Implementar cartão de crédito</Card.Title>
                 </Card.Header>
                 <Card.Footer>
-                  <button type="button" className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                  <Button>
                     <ThumbsUp className="size-3" />
                     <span className="text-sm">12</span>
-                  </button>
-                  <button type="button" className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                  </Button>
+                  <Button>
                     <MessageCircleIcon className="size-3" />
                     <span className="text-sm">12</span>
-                  </button>
+                  </Button>
                 </Card.Footer>
               </Card.Root>
           </Section.Content>
         </Section.Root>
       </main>
-    </div>
   )
 }
