5eb1d3564551041642d0b55c3d96bb652d8f2abf
Pedro Henrique <ph.andradesousa31@gmail.com>
2026-09-07 02:48:42 -0300

    feat: add issue modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,36 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { NuqsAdapter } from "nuqs/adapters/next/app"
+import { NuqsAdapter } from "nuqs/adapters/next"
 import { ReactQueryProvider } from "@/lib/react-query"
 
 export const metadata: Metadata = {
   title: {
     template: "%s | Product Roadmap",
-    default: "Product Roadmap"
+    default: "Product Roadmap",
   },
-  description: "Follow the development progress of our entire platform",
+  description: "Follow the development progress of our entire platform.",
 }
 
 const interFont = Inter({ subsets: ["latin"] })
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode
+  modal: React.ReactNode
+}>) {
   return (
     <html lang="en" className={interFont.className}>
       <body className="bg-navy-950 text-navy-50 antialiased">
         <ReactQueryProvider>
-          <NuqsAdapter>{children}</NuqsAdapter>
+          <NuqsAdapter>
+            {modal}
+            {children}
+          </NuqsAdapter>
         </ReactQueryProvider>
-        </body>
+      </body>
     </html>
   )
 }
